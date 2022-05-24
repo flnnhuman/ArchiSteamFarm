cf0ef70380981ed1d7051c4528f3e6ed7f86ba3a
vpeseckiy <v.peseckiy@softteco.com>
2022-05-24 10:35:00 +0300

    all

--- ArchiSteamFarm/Steam/Security/Confirmation.cs
+++ ArchiSteamFarm/Steam/Security/Confirmation.cs
@@ -26,20 +26,24 @@ using Newtonsoft.Json;
 
 namespace ArchiSteamFarm.Steam.Security;
 
-public sealed class Confirmation {
+public class Confirmation {
 	[JsonProperty(Required = Required.Always)]
-	public ulong Creator { get; }
+	public ulong Creator { get; set; }
 
 	[JsonProperty(Required = Required.Always)]
-	public ulong ID { get; }
+	public ulong ID { get; set; }
 
 	[JsonProperty(Required = Required.Always)]
-	public ulong Key { get; }
+	public ulong Key { get; set; }
 
 	[JsonProperty(Required = Required.Always)]
-	public EType Type { get; }
+	public EType Type { get; set; }
 
-	internal Confirmation(ulong id, ulong key, ulong creator, EType type) {
+	public Confirmation() {
+
+	}
+
+	public Confirmation(ulong id, ulong key, ulong creator, EType type) {
 		ID = id > 0 ? id : throw new ArgumentOutOfRangeException(nameof(id));
 		Key = key > 0 ? key : throw new ArgumentOutOfRangeException(nameof(key));
 		Creator = creator > 0 ? creator : throw new ArgumentOutOfRangeException(nameof(creator));
